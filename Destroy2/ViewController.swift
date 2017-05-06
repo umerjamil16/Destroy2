e7158786287dc869a51e54d7c5d529d8e80a6867
Umer <umerjamil@protonmail.com>
2017-05-06 16:17:30 +0500

    Initial Commit

---
+++ Destroy2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Destroy2
+//
+//  Created by Umer on 06/05/2017.
+//  Copyright © 2017 Umer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
